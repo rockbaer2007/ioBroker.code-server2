1af099da35498ef0503055f0efd84c9d354e4839
rockbaer2007 <rockbaer2007@live.de>
2025-05-29 18:37:09 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'code-server2 adapter settings': {
+        'en': 'Adapter settings for code-server2',
+        'de': 'Adaptereinstellungen für code-server2',
+        'ru': 'Настройки адаптера для code-server2',
+        'pt': 'Configurações do adaptador para code-server2',
+        'nl': 'Adapterinstellingen voor code-server2',
+        'fr': "Paramètres d'adaptateur pour code-server2",
+        'it': "Impostazioni dell'adattatore per code-server2",
+        'es': 'Ajustes del adaptador para code-server2',
+        'pl': 'Ustawienia adaptera dla code-server2',
+        'uk': 'Налаштування адаптера для code-server2',
+        'zh-cn': 'code-server2的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option1',
+        'ru': 'вариант1',
+        'pt': 'opção1',
+        'nl': 'Option1',
+        'fr': 'option1',
+        'it': 'opzione1',
+        'es': 'Opción1',
+        'pl': 'opcja 1',
+        'uk': 'Варіант1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option2',
+        'ru': 'вариант2',
+        'pt': 'opção2',
+        'nl': 'Optie2',
+        'fr': 'Option2',
+        'it': 'opzione2',
+        'es': 'opción 2',
+        'pl': 'opcja2',
+        'uk': 'Варіант2',
+        'zh-cn': '选项2'
+    }
+};
